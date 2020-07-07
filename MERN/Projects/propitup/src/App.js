bea2470ebbebaa0d3e480a4292f3b2a51cb8064e
Bryce Alderete <brycealderete@gmail.com>
2020-07-07 14:59:52 -0700

    mern

--- MERN/Projects/propitup/src/App.js
+++ MERN/Projects/propitup/src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Bryce"} lastName={"Alderete"} age={"31"} hairColor={"Brown"}/>
-      <PersonCard firstName={"Destinee"} lastName={"Alderete"} age={"28"} hairColor={"Blonde"}/>
-      <PersonCard firstName={"Hector"} lastName={"Raygoza"} age={"50"} hairColor={"Brown"}/>
-      <PersonCard firstName={"Gina"} lastName={"Raygoza"} age={"52"} hairColor={"Brown"}/>
+      <PersonCard firstName={"Bryce"} lastName={"Alderete"} age={31} hairColor={"Brown"}/>
+      <PersonCard firstName={"Destinee"} lastName={"Alderete"} age={8} hairColor={"Blonde"}/>
+      <PersonCard firstName={"Hector"} lastName={"Raygoza"} age={50} hairColor={"Brown"}/>
+      <PersonCard firstName={"Gina"} lastName={"Raygoza"} age={52} hairColor={"Brown"}/>
         
     </div>
   );
--- MERN/Projects/propitup/src/components/PersonCard.js
+++ MERN/Projects/propitup/src/components/PersonCard.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            Age: props.age
+        };
+    }
+        
+    addYear =()=>{
+        this.setState({Age: this.state.Age+1});
+    }
+
     render(){
         return(
             <div>
                 <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.Age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.addYear}>Birthday button for {this.props.firstName} {this.props.lastName}</button>
 
                 
 
